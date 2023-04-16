05c83acda88bc2a88212180a8776f1e7a719306e
Jonathan Leibovich <60627566+leamur88@users.noreply.github.com>
2023-04-16 17:18:07 -0500

    added buttons for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,11 @@ export default function Home() {
 			</Link>
 
 	</Box>
+	<Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, textAlign: "center", justifyContent: "center", mt: 4, flexDirection: "column" }}>
+			
+			<Link href={"/arrow"}><Button sx={{my: 1}} color="success" variant="contained" size="large">Hello! Click here to go to the rotation demo!</Button></Link>
+			<Link href={"/pinch"}><Button color="error" sx={{my: 2}} variant="contained" size="large">Hello! Click here to go to the pinch demo!</Button></Link>
+	</Box>
 		
 	</>)
 }
