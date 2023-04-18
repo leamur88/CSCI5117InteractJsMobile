9e7dfe68fe536143bcf7440ec81be46905f8628f
Jonathan Leibovich <60627566+leamur88@users.noreply.github.com>
2023-04-18 13:45:10 -0500

    added vincent and jefferson

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import { Button, Box } from '@mui/material';
+import { purple } from '@mui/material/colors';
+
 
 export default function Home() {
 	return(<>
@@ -9,15 +11,20 @@ export default function Home() {
 				<Button variant="contained" sx={{mx: 2}} color="success">Hello! Click here to go to the rotation demo!</Button>
 			</Link>
 
-			<Link href="/pinch">
-				<Button variant="contained" sx={{mx: 2}} color="error">Hello! Click here to go to the pinch demo!</Button>
+			<Link href="/pinchJefferson">
+				<Button variant="contained" sx={{mx: 2}} color="secondary">Hello! Click here to go to the Jefferson pinch demo!</Button>
+			</Link>
+
+			<Link href="/pinchStarry">
+				<Button variant="contained" sx={{mx: 2}} color="error">Hello! Click here to go to the Vincent van Gogh pinch demo (buggy)!</Button>
 			</Link>
 
 	</Box>
 	<Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, textAlign: "center", justifyContent: "center", mt: 4, flexDirection: "column" }}>
 			
 			<Link href={"/arrow"}><Button sx={{my: 1}} color="success" variant="contained" size="large">Hello! Click here to go to the rotation demo!</Button></Link>
-			<Link href={"/pinch"}><Button color="error" sx={{my: 2}} variant="contained" size="large">Hello! Click here to go to the pinch demo!</Button></Link>
+			<Link href={"/pinchJefferson"}><Button color="secondary" sx={{my: 2}} variant="contained" size="large">Hello! Click here to go to the Jefferson pinch demo!</Button></Link>
+			<Link href={"/pinchStarry"}><Button color="error" sx={{my: 2}} variant="contained" size="large">Hello! Click here to go to the Vincent van Gogh pinch demo (buggy)!</Button></Link>
 	</Box>
 		
 	</>)
